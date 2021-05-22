b9a4ac4ad8faf20b1522140ce688bc73674137f0
GusNitrous <gusnitrous@gmail.com>
2021-05-22 16:02:58 +0300

    Feature: uploading files.

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ReviewModule } from './review/review.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { getMongoConfig } from './configs/mongo.config';
+import { FilesModule } from './files/files.module';
 
 @Module({
 	imports: [
@@ -19,6 +20,7 @@ import { getMongoConfig } from './configs/mongo.config';
 		TopPageModule,
 		ProductModule,
 		ReviewModule,
+		FilesModule,
 	],
 })
 export class AppModule {}
